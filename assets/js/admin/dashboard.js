cb68bbf73c8fb0d4102a3ae6c8d33fcf62ff76be
Juste Niyonteze <justirenee10@gmail.com>
2022-01-26 07:53:57 +0200

    dashboard users fix

--- assets/js/admin/dashboard.js
+++ assets/js/admin/dashboard.js
@@ -1,17 +1,11 @@
-let queries = localStorage.getItem('queries')
-	? JSON.parse(localStorage.getItem('queries'))
-	: []
-let posts = localStorage.getItem('posts')
-	? JSON.parse(localStorage.getItem('posts'))
-	: []
-let users = localStorage.getItem('users')
-	? JSON.parse(localStorage.getItem('users'))
-	: []
+const allQueries = getLocalArrayOf('queries')
+const allPosts = getLocalArrayOf('posts')
+const allUsers = getLocalArrayOf('users')
 
 const queriesTotal = document.getElementById('queries')
 const postsTotal = document.getElementById('posts')
 const usersTotal = document.getElementById('users')
 
-queriesTotal.appendChild(document.createTextNode(queries.length))
-postsTotal.appendChild(document.createTextNode(posts.length))
-usersTotal.appendChild(document.createTextNode(users.length))
+queriesTotal.appendChild(document.createTextNode(allQueries.length))
+postsTotal.appendChild(document.createTextNode(allPosts.length))
+usersTotal.appendChild(document.createTextNode(allUsers.length))
